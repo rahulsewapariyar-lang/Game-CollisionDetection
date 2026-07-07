977064e305c5e69c074f91dabd7bbd3905bf5be9
rahul161616 <rahul1616@gmail.com>
2026-07-08 00:17:09 +0545

    GLFW, GLAD, VBO and VAO

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,31 @@ int main() {
 		glfwTerminate();
 		return -1;
 	}
+
+	float vertices[] =
+	{
+		-0.5f, -0.5f,
+		 0.5f, -0.5f,
+		 0.0f,  0.5f
+	};
+
+	GLuint VBO; //vbo lives in CPU ram
+	glGenBuffers(1, &VBO); //1 represents the number of buffers to generate, &VBO is the address of the buffer object name to be generated
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); //bind the buffer object to the GL_ARRAY_BUFFER target so that it can be used for vertex attribute data
+
+
+	GLuint VAO; //cpu variable
+	glGenVertexArrays(1, &VAO); //creates a VAO object and stores its ID in the VAO variable
+	glBindVertexArray(VAO); // make it the current VAO
+
+
+
+	//this here will copy the data from the CPU ram to the GPU ram so that it can be used for rendering
+	glBufferData(GL_ARRAY_BUFFER,
+		sizeof(vertices),
+		vertices,//the data to be copied to the buffer object
+		GL_STATIC_DRAW); //"I'm probably going to upload this once and draw it many times."
+
 	glViewport(0, 0, 800, 600);
 
 	std::cout << "GLAD initialized successfully!\n";
